00f73aebff27ec13fc4db6a69337ca2360fc4d85
trendeltrep <maxim.krikukha20031@gmail.com>
2025-04-29 19:19:18 +0300

    feat: added posts and comms with auto seed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './posts/post.entity';
+import { Comment } from './comments/comment.entity';
+import { PostsModule } from './posts/post.module';
+import { CommentsModule } from './comments/comment.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '12345',
+      database: 'postgres',
+      entities: [Post, Comment],
+      synchronize: true,
+    }),
+    PostsModule,
+    CommentsModule,
+  ],
 })
 export class AppModule {}

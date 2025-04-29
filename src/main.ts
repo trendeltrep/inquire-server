9a4c2ac8cd9bd16e11c8c08961058e9dffc115c6
trendeltrep <maxim.krikukha20031@gmail.com>
2025-04-29 22:46:24 +0300

    feat: added env

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000',
+      'https://inquire-client-9ehhmjtet-maksyms-projects-e7f962b7.vercel.app/', 
+      'https://inquire-client-coral.vercel.app/'],
   });
-  
+
   app.setGlobalPrefix('api');
 
   const postsService = app.get(PostsService);
